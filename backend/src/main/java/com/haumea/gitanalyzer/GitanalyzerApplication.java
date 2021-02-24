076393d80b01773bf6a6aec17d07e486cd11921b
tmbui <tmbui@sfu.ca>
2021-02-24 03:32:16 -0800

    add login and logout endpoints

--- backend/src/main/java/com/haumea/gitanalyzer/GitanalyzerApplication.java
+++ backend/src/main/java/com/haumea/gitanalyzer/GitanalyzerApplication.java
@@ -1,10 +1,11 @@
 package com.haumea.gitanalyzer;
 
+import org.jasig.cas.client.boot.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
+@EnableCasClient
 public class GitanalyzerApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/com/haumea/gitanalyzer/controller/CASController.java
@@ -0,0 +1,40 @@
+package com.haumea.gitanalyzer.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path = "/api/v1/cas")
+@Slf4j
+public class CASController {
+
+    @Value("${cas.server-url-prefix}")
+    private String CASUrl;
+
+    @GetMapping("/login")
+    public String getCASUserId(HttpServletRequest request){
+        String UserId = request.getRemoteUser();
+
+        return UserId;
+    }
+
+    @GetMapping("/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        if(session != null || !request.isRequestedSessionIdValid()) {
+            String id = session.getId();
+            session.invalidate();
+            log.info("JSESSIONID: " + id + " is valid: " + request.isRequestedSessionIdValid());
+        }
+
+        response.sendRedirect(CASUrl + "/logout");
+    }
+}
